c2fbbae7a9d8f08e90cf90ea908d45f000593164
momoran <haiyuns@qq.com>
2021-03-31 21:35:35 +0800

    增加公共头文件

---
+++ include/CommonInterferce.h
@@ -0,0 +1,15 @@
+// 系统库
+#include <memory>
+#include <map>
+#include <string>
+#include <iostream>
+#include <stdint.h>
+
+// 公共组件，抽象虚基类和公共变量定义
+#include "operateLib.h"
+#include "std_lib.h"
+
+// 多态库
+#include "SubArray.h"
+#include "HammingWeight.h"
+#include "MatchAdd.h"
