364d3c1af734fb49234045c1354c02fc5e12375b
momoran <haiyuns@qq.com>
2021-04-14 01:07:39 +0800

    利用set集合求第几个丑数

--- source/UnglyNumberNo.cpp
+++ source/UnglyNumberNo.cpp
@@ -1,6 +1,7 @@
 #include "UnglyNumberNo.h"
 #include <iostream>
 #include <set>
+#include <algorithm>
 
 UnglyNumberNo::UnglyNumberNo()
 {
@@ -21,7 +22,7 @@ WORD UnglyNumberNo::execute()
 
 int UnglyNumberNo::nthUglynumber(int n)
 {
-    int index = 0;
+/*    int index = 0;
     while (n > 0) {
         index++;
         if (isUgly(index)) {
@@ -30,8 +31,32 @@ int UnglyNumberNo::nthUglynumber(int n)
         }
     }
     std::cout << "next number is:" << index << std::endl;
-
-    return index;
+*/
+    std::set<int> lib = {1, 2, 3};
+    std::set<int> tmpResult = {2, 3, 5, 4, 6, 10, 6, 9, 15};
+    auto size = lib.size();
+    while (size < n)
+    {
+        auto it = find_if(tmpResult.begin(), tmpResult.end(), [&](int temp) -> bool{
+            return temp > *(lib.rbegin());
+        });
+        long tempNum = *it;
+        lib.insert(static_cast<int>(tempNum));
+        int array2[] = {tempNum * 2, tempNum * 3, tempNum * 5};
+        tmpResult.insert(array2, array2 + 3);
+        std::cout << "number:" << *it << "last:" << *(lib.rbegin()) << std::endl;
+        size = lib.size();
+    }
+    auto first = lib.begin();
+    int index = 1;
+    while(first != lib.end()) {
+        if (index == n) {
+            return *first;
+        }
+        index++;
+        first++;
+    }
+    return 0;
 }
 
 void UnglyNumberNo::UpdateTestCase()
