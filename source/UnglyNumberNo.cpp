d3bd708dd3a6d85e4018ace3e61ed264a3aeb091
momoran <haiyuns@qq.com>
2021-04-14 00:08:18 +0800

    丑数调试

--- source/UnglyNumberNo.cpp
+++ source/UnglyNumberNo.cpp
@@ -21,7 +21,7 @@ WORD UnglyNumberNo::execute()
 
 int UnglyNumberNo::nthUglynumber(int n)
 {
-    /*int index = 0;
+    int index = 0;
     while (n > 0) {
         index++;
         if (isUgly(index)) {
@@ -29,23 +29,9 @@ int UnglyNumberNo::nthUglynumber(int n)
             std::cout << "number is:" << index << std::endl;
         }
     }
-    std::cout << "next number is:" << index << std::endl;*/
-    int array[3] = {1, 2, 3};
-    int array1[3] = {2, 3, 5};
-    std::set<int> lib(array, array + 3);
-    int size = lib.size();
-    //while (size <= n) {
-        auto last = lib.end();
-        int index = 0;
-        while (index < 3) {
-            last--;
-            array1[index] = (*last) * 2;
-            std::cout << "number is: " << array1[index] << std::endl;
-            index ++;
-        }
-        
-    //}
-    return 1;
+    std::cout << "next number is:" << index << std::endl;
+
+    return index;
 }
 
 void UnglyNumberNo::UpdateTestCase()
