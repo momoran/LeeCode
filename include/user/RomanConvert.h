bf1b5bde6f6c4cb67e51aa7e2ccc2e80382211eb
momoran <haiyuns@qq.com>
2021-05-15 10:45:53 +0800

    罗马数转换

--- include/user/RomanConvert.h
+++ include/user/RomanConvert.h
@@ -4,21 +4,32 @@
 #include "operateLib.h"
 #include <string>
 #include <map>
+
 namespace {
     std::map<int, std::string> LOOK_TABLE = {
-        {1, "I"},
-        {4, "IV"},
-        {5, "V"},
-        {9, "IX"},
-        {10, "X"},
-        {40, "XL"},
-        {50, "L"},
-        {90, "XC"},
-        {100, "C"},
-        {400, "CD"},
-        {500, "D"},
-        {900, "CM"},
-        {1000, "M"},
+        {1,    "I" },
+        {4,    "IV"},
+        {5,    "V" },
+        {9,    "IX"},
+        {10,   "X" },
+        {40,   "XL"},
+        {50,   "L" },
+        {90,   "XC"},
+        {100,  "C" },
+        {400,  "CD"},
+        {500,  "D" },
+        {900,  "CM"},
+        {1000, "M" },
+    };
+
+    std::map<char, int> OTHER_TABLE = {
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000}
     };
 };
 
@@ -33,10 +44,15 @@ public:
     
 private:
     std::string intToRoman(int num);
+    int romanToInt(std::string s);
     std::string GetSameString(std::string str, int cnt);
-    int m_num;
-    std::string result;
-    std::string hopeResult;
+    int romanToInt2(std::string s);
+    // int m_num;
+    // std::string result;
+    // std::string hopeResult;
+    std::string m_str;
+    int result;
+    int hopeResult;
 };
 
 #endif
--- source/user/RomanConvert.cpp
+++ source/user/RomanConvert.cpp
@@ -1,10 +1,10 @@
 #include "RomanConvert.h"
 #include <iostream>
-
+#include <cstring>
 WORD RomanConvert::execute()
 {
     UpdateTestCase();
-    result = intToRoman(m_num);
+    result = romanToInt2(m_str);
     return VerifyResult();
 }
 
@@ -30,10 +30,44 @@ std::string RomanConvert::GetSameString(std::string str, int cnt)
     return tmpStr;
 }
 
+int RomanConvert::romanToInt(std::string s)
+{ 
+    int tmp = 0;
+    std::string tmpStr = s;
+    auto last = LOOK_TABLE.rbegin();
+    while(last != LOOK_TABLE.rend()) {
+        while(!tmpStr.empty()) {
+            if(strncmp(tmpStr.c_str(), last->second.c_str(), last->second.size()) != 0) {
+                break;
+            }
+            tmp += last->first;
+            tmpStr.erase(0, last->second.size());
+            std::cout << last->first << " " << tmp << std::endl;
+        }
+        last++;
+    }
+    std::cout << tmp << std::endl;
+    return tmp;
+}
+
+int RomanConvert::romanToInt2(std::string s)
+{
+    int tmp = 0;
+    int strLen = s.size();
+    for(auto i = 0; i < strLen; i++) {
+        if (i < (strLen - 1) && OTHER_TABLE.at(s[i]) < OTHER_TABLE.at(s[i+1])) {
+            tmp -= OTHER_TABLE.at(s[i]);
+        } else {
+            tmp += OTHER_TABLE.at(s[i]);
+        }
+    }
+    return tmp;
+}
+
 void RomanConvert::UpdateTestCase()
 {
-    m_num = 3;
-    hopeResult = "III";
+    m_str = "LVIII";
+    hopeResult = 58;
 }
 
 WORD RomanConvert::VerifyResult()
