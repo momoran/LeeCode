736cbe2121244bdcf52b4f46b5f68b9bcae29981
momoran <haiyuns@qq.com>
2021-05-22 17:56:45 +0800

    地图最小路径长度，路径数

--- source/user/DynamicProgram.cpp
+++ source/user/DynamicProgram.cpp
@@ -7,13 +7,70 @@
 WORD DynamicProgram::execute()
 {
     UpdateTestCase();
-    result = longestPalindrome(m_inputStr);
+    // result = uniquePaths(m_mAxies,m_nAxies);
+    // result = uniquePathsWithObstacles(m_obstacleGrid);
+    result = minPathSum(m_Grid);
     return VerifyResult();
 }
 
-int DynamicProgram::MaxChildArrayOrder(std::vector<int> arr)
+int DynamicProgram::uniquePaths(int m, int n)
 {
+    std::vector<std::vector<int>> dpArray(m, std::vector<int>(n));
+    for(int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            dpArray[i][j] = (i == 0 || j == 0) ? 1 : dpArray[i][j-1] + dpArray[i-1][j];
+        }
+    }
+    std::cout << dpArray[m-1][n-1] << std::endl;
+    return dpArray[m-1][n-1];
+}
 
+int DynamicProgram::uniquePathsWithObstacles(std::vector<std::vector<int>>& obstacleGrid)
+{
+    std::vector<std::vector<int>> dpArray = obstacleGrid;
+    int width = obstacleGrid.size();
+    int length = obstacleGrid[0].size();
+    dpArray[0][0] = 1;
+    for(int i = 0; i < width; i++) {
+        for (int j = 0; j < length; j++) {
+            if (obstacleGrid[i][j] == 1) {
+                dpArray[i][j] = 0;
+            } else {
+                if ((i == 0 || j == 0) && i != j) {
+                    // 由于有障碍无的存在，因此沿墙壁方向的路径数与前一个坐标保持一致
+                    dpArray[i][j] = (i == 0) ? dpArray[i][j-1] : dpArray[i-1][j];
+                } else {
+                    if (i == 0) {
+                        //(i == 0 || j == 0) && i != j 该条件将坐标00进行了过滤，因此需要单独处理
+                        continue;
+                    }
+                    dpArray[i][j] = dpArray[i][j-1] + dpArray[i-1][j];
+                }
+                
+            }
+        }
+    }
+    return dpArray[width-1][length-1];
+}
+
+int DynamicProgram::minPathSum(std::vector<std::vector<int>>& grid)
+{
+    std::vector<std::vector<int>> dpArray = grid;
+    int width = grid.size();
+    int length = grid[0].size();
+    for(int i = 0; i < width; i++) {
+        for (int j = 0; j < length; j++) {
+            if((i == 0 || j == 0) && (i != j)) {
+                dpArray[i][j] += (i == 0) ? dpArray[i][j-1] : dpArray[i-1][j];
+            } else {
+                if (i == 0) {
+                    continue;
+                }
+                dpArray[i][j] += dpArray[i-1][j] > dpArray[i][j-1] ? dpArray[i][j-1] : dpArray[i-1][j];
+            }
+        }
+    }
+    return dpArray[width-1][length-1];
 }
 
 std::string DynamicProgram::longestPalindrome(std::string s)
@@ -76,21 +133,13 @@ std::string DynamicProgram::longestPalindrome(std::string s)
     return s.substr(maxBegin, maxLen);
 }
 
-bool DynamicProgram::IsPalindrome(std::string s)
-{
-    //std::cout << s << std::endl;
-    for(int left = 0, right = s.size() - 1; left < right; left++, right--) {
-        if(s[left] != s[right]) {
-            return false;
-        }
-    }
-    return true;
-}
-
 void DynamicProgram::UpdateTestCase()
 {
-    m_inputStr = "cbbd";
-    hopeResult = "bb";
+    // 无障碍路径数
+    // m_mAxies = 3;
+    // m_nAxies = 7;
+    m_Grid = {{1,2,3},{4,5,6}};
+    hopeResult = 12;
 }
 
 WORD DynamicProgram::VerifyResult()
